3763af514dcbe0993acf3b0fbe396e781eac8c0d
unentero <emmocampo1010@gmail.com>
2026-06-20 00:40:11 -0300

    logica hasta ej 25

--- practica-logica.pl
+++ practica-logica.pl
@@ -64,17 +64,60 @@ ejercicio_18(L,E):- media(L,M),maximo(L,MAX),minimo(L,MIN),E = [M,MAX,MIN].
 %-------------------------------Nivel 3-----------------------------------
 %19. Escriba una función que calcule el i-ésimo número perfecto (los números perfectos son aquellos 
 %que son iguales a la suma de sus divisores).
-
+lista_divisores(_, 0, []) :- !.
+lista_divisores(X,N,L):- N>0, N1 is N-1, lista_divisores(X,N1,L1),(X mod N =:= 0 -> L = [N|L1]; L = L1).
+es_numero_perfecto(X):- X>1, X1 is X-1,lista_divisores(X,X1,L),sumatoria(L,S),X =:= S.
+iesimo_perfecto_desde(X,I,P):-
+(I=:= 1,es_numero_perfecto(X)
+-> P is X
+; es_numero_perfecto(X)
+-> X1 is X+1, I1 is I-1,iesimo_perfecto_desde(X1,I1,P)
+; X1 is X+1,
+iesimo_perfecto_desde(X1,I,P)
+).
+iesimo_perfecto(I,P):-iesimo_perfecto_desde(1,I,P).
 %20. Escriba una función que calcule los n primeros números primos y los devuelva en una lista.
-
+lista_n_divisores_primos(_,0,[]):- !.
+lista_n_divisores_primos(1,_,L):- L = [1].
+lista_n_divisores_primos(X,N,L):-(X mod N =:= 0, X =\= N, N =\= 1 
+->N1 is N-1, lista_n_divisores_primos(X,N1,L1),L=[N|L1]
+;N1 is N-1, lista_n_divisores_primos(X,N1,L1),L = L1).
+es_primo(X):-lista_n_divisores_primos(X,X,[]).
+lista_n_primos_desde(_,0,[]):- !.
+lista_n_primos_desde(X,N,L):-(es_primo(X)
+->X1 is X+1,N1 is N-1, lista_n_primos_desde(X1,N1,L1),L=[X|L1]
+;X1 is X+1, lista_n_primos_desde(X1,N,L1),L = L1).
+lista_n_primos(N,L):-lista_n_primos_desde(1,N,L).
 %21. Escriba una función que determine la Varianza de una lista de números: 
 %Varianza = Sumatoria (Xi - Media)2 / (n - 1). Siendo Xi cada uno de los n elementos de la lista.
-
+sumatoria_varianza([],SV):- SV is 0.
+sumatoria_varianza([H|T],SV):- media([H|T],M),sumatoria_varianza(T,SV1),SV is (H-M)**2 + SV1.
+varianza(L,V):-sumatoria_varianza(L,SV),cantidad(L,C),V is SV/(C-1).
 %22. Escriba una función que calcule la Moda de una lista de números (el número que más se repite).
-
+contar_instancias_de(_,[],0).
+contar_instancias_de(X,[X|T],C):- contar_instancias_de(X,T,C1), C is C1 + 1.
+contar_instancias_de(X,[H|T],C):- X \= H, contar_instancias_de(X,T,C1), C is C1.
+mas_repetido([],M,_,M).
+mas_repetido([H|T],M,L,R):-(contar_instancias_de(H,L,C1),contar_instancias_de(M,L,C2),C1>C2 -> mas_repetido(T,H,L,R);mas_repetido(T,M,L,R)).
+moda([H|T],M) :- mas_repetido(T,H,[H|T],M).
 %23. Devuelva la cantidad de números que contiene una lista.
-
+cantidad_numeros_en([],0).
+cantidad_numeros_en([H|T],C):-(number(H) -> cantidad_numeros_en(T,C1),C is C1+1;cantidad_numeros_en(T,C)).
 %24. Realice una función que transforme un binario, expresado a través de una lista de {0,1}, 
 %en decimal.
-
+tamanio([],0).
+tamanio([_|T],R):- tamanio(T,R1),R is 1 + R1.
+binario_a_decimal([],0).
+binario_a_decimal([H|T],D):- tamanio(T,R),binario_a_decimal(T,D1), D is H*(2**R) + D1. 
 %25. Realice una función que sume dos números binarios, expresados a través de dos listas de {0,1}.
+suma_binaria([],[],0,[]).
+suma_binaria([],[],1,[1]).
+suma_binaria([1|T1],[1|T2],1,R):-suma_binaria(T1,T2,1,R1),R=[1|R1].
+suma_binaria([1|T1],[1|T2],0,R):-suma_binaria(T1,T2,1,R1),R=[0|R1].
+suma_binaria([1|T1],[0|T2],1,R):-suma_binaria(T1,T2,1,R1),R=[0|R1].
+suma_binaria([1|T1],[0|T2],0,R):-suma_binaria(T1,T2,0,R1),R=[1|R1].
+suma_binaria([0|T1],[1|T2],1,R):-suma_binaria(T1,T2,1,R1),R=[0|R1].
+suma_binaria([0|T1],[1|T2],0,R):-suma_binaria(T1,T2,0,R1),R=[1|R1].
+suma_binaria([0|T1],[0|T2],1,R):-suma_binaria(T1,T2,0,R1),R=[1|R1].
+suma_binaria([0|T1],[0|T2],0,R):-suma_binaria(T1,T2,0,R1),R=[0|R1].
+suma_binaria_definitiva(L1,L2,LR):-reverse(L1,L1I),reverse(L2,L2I),suma_binaria(L1I,L2I,0,LRI),reverse(LRI,LR).
